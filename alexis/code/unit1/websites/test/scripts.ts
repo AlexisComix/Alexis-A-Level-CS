d5062ac3806a676cdfd8de2935ca68e9ce712e57
AlexisComix <thecomixguy@hotmail.com>
2022-10-06 08:54:24 +0100

    Add Test Website

---
+++ alexis/code/unit1/websites/test/scripts.ts
@@ -0,0 +1,9 @@
+function button() {
+    // This is a test function for a button.
+    alert("DELL SUCKS !!!!!");
+}
+
+function styled_button() {
+    // This is a test function for a css styled button
+    alert("Le funny >:3")
+}
