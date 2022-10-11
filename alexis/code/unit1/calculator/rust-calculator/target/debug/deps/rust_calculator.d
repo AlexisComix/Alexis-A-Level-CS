b8976e51a7b1897751be314c7d7e9c59400a716b
AlexisComix <thecomixguy@hotmail.com>
2022-10-11 15:29:50 +0100

    Change calculator in rust to use Cargo

---
+++ alexis/code/unit1/calculator/rust-calculator/target/debug/deps/rust_calculator.d
@@ -0,0 +1,5 @@
+C:\Users\Theco\Documents\A-Level CS\Alexis-A-Level-CS\alexis\code\unit1\calculator\rust-calculator\target\debug\deps\rust_calculator.exe: src\main.rs
+
+C:\Users\Theco\Documents\A-Level CS\Alexis-A-Level-CS\alexis\code\unit1\calculator\rust-calculator\target\debug\deps\rust_calculator.d: src\main.rs
+
+src\main.rs:
---
+++ alexis/code/unit1/calculator/rust-calculator/target/debug/rust-calculator.d
@@ -0,0 +1,1 @@
+C:\Users\Theco\Documents\A-Level\ CS\Alexis-A-Level-CS\alexis\code\unit1\calculator\rust-calculator\target\debug\rust-calculator.exe: C:\Users\Theco\Documents\A-Level\ CS\Alexis-A-Level-CS\alexis\code\unit1\calculator\rust-calculator\src\main.rs
