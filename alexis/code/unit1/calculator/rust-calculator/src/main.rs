ebfdd7c3113e0b7d3e12270cf316e51b8511ac81
AlexisComix <thecomixguy@hotmail.com>
2022-11-02 11:25:55 +0000

    Change to use f64 and fix unused variable warning

--- alexis/code/unit1/calculator/rust-calculator/src/main.rs
+++ alexis/code/unit1/calculator/rust-calculator/src/main.rs
@@ -5,10 +5,10 @@ fn main() {
     let operatornum: u8 = get_operator();
     if operatornum == 0 {return}    // Funky guard clause
 
-    let first_operand: f32 = get_operand();
-    let second_operand: f32 = get_operand();
+    let first_operand: f64 = get_operand();
+    let second_operand: f64 = get_operand();
 
-    let result: f32 = do_operation(operatornum, first_operand, second_operand);
+    let result: f64 = do_operation(operatornum, first_operand, second_operand);
     println!("Result: {}", result);
 }
 
@@ -30,18 +30,17 @@ fn get_operator() -> u8 {
     return 0;   // Fail
 }
 
-fn get_operand() -> f32 {
-    let mut num: f32 = 0.0;
+fn get_operand() -> f64 {
     let mut input = String::new();
     
     println!("Enter number: ");
     io::stdin().read_line(&mut input).expect("Not a valid string");
-    num = input.trim().parse().expect("Not a valid number");
+    let num: f64 = input.trim().parse().expect("Not a valid number");
     
     return num;
 } 
 
-fn do_operation(operation: u8, operand1: f32, operand2: f32) -> f32 {
+fn do_operation(operation: u8, operand1: f64, operand2: f64) -> f64 {
     if operation == 1 {return operand1 + operand2}
     else if operation == 2 {return operand1 - operand2}
     else if operation == 3 {return operand1 * operand2}
