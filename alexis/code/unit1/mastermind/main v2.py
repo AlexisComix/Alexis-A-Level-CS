8247875b4e9d15535d75c2853952ed2fe80005fa
AlexisComix <thecomixguy@hotmail.com>
2022-11-14 10:30:18 +0000

    Do some work on Mastermind

--- alexis/code/unit1/mastermind/main v2.py
+++ alexis/code/unit1/mastermind/main v2.py
@@ -1,15 +1,46 @@
+# This code can only be described as spaghetti code.
+# I have taken Python and made it into Java.
+# Why?
+
 from dataclasses import dataclass
 import sys
 import os
 from random import randint
 import pygame
 
+COLOURS = (
+    BLACK := (0, 0, 0),
+    WHITE := (255, 255, 255),
+    GREY := (150, 150, 150),
+    RED := (255, 0, 0),
+    GREEN := (0, 255, 0),
+    BLUE := (0, 0, 255),
+    YELLOW := (255, 255, 0),
+    PURPLE := (175, 0, 255),
+    PINK := (255, 120, 165),
+    BROWN := (103, 65, 15),
+    LIGHT_BROWN := (104, 86, 61)
+)
+
 class Board:
     """
     The class for the board and the properties of the board.
     Handles events relating to the board
     """
-    
+    def __init__(self):
+        pass
+
+    def draw(self, screen: pygame.Surface):
+        width = screen.get_width()
+        height = screen.get_height()
+
+        screen.fill(LIGHT_BROWN)
+
+        startx = width // 6
+        boardwidth = width - (2 * startx)
+        boardrect = pygame.rect.Rect(startx, 0, boardwidth, height)
+        pygame.draw.rect(screen, BROWN, boardrect)
+
 
 @dataclass
 class Counter:
@@ -68,7 +99,7 @@ class Window:
         self.height = 600
         self.dimensions = (self.width, self.height)
         
-        pygame.display.set_mode(self.dimensions, vsync=1)
+        self.win = pygame.display.set_mode(self.dimensions, vsync=1)
         pygame.display.set_caption("Alexis' Mastermind", "Alexis' Mastermind")
 
         pygame.mixer.init()
@@ -81,7 +112,10 @@ class Window:
     def set_assets(self):
         icon = self.icon.load_asset()
         if icon is not None:
-            pygame.display.set_icon(icon)
+            pygame.display.set_icon(icon)  # type: ignore
+
+    def get_win(self):
+        return self.win
 
 
 class Game:
@@ -92,7 +126,12 @@ class Game:
         self.running = True
         self.clock = pygame.time.Clock()
         self.window = Window()
+        self.WIN = self.window.get_win()
+        self.board = Board()
         self.main()
+
+    def draw(self):
+        self.board.draw(self.WIN)
 
     def main(self):
         while self.running:
@@ -102,6 +141,7 @@ class Game:
                     pygame.quit()
                     sys.exit()
 
+        self.draw()
         pygame.display.flip()
         self.clock.tick(60)
 
