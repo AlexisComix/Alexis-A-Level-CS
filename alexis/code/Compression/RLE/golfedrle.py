301807cc50111dd9ac8ac22fe46fe009bb282ae9
AlexisComix <Thecomixguy@hotmail.com>
2023-02-19 19:28:33 +0000

    GOLF IT HARDER

--- alexis/code/Compression/RLE/golfedrle.py
+++ alexis/code/Compression/RLE/golfedrle.py
@@ -1,11 +1,9 @@
-from itertools import groupby;r=lambda s:[(c,len(list(g)))for c,g in groupby(s)];f=lambda s:"".join([f"{i}{j}"for i,j in r(s)]);
+from itertools import groupby;f=lambda s:"".join([f"{i}{j}"for i,j in[(c,len(list(g)))for c,g in groupby(s)]]);
 
 # TESTS
 def main():
     some_str = "11111110000000000101010000000"
-    rle = r(some_str)
     fancy = f(some_str)
-    print(rle)
     print(fancy)
     print(compression_ratio(some_str, fancy))
 
