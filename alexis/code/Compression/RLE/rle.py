6f0930eb6708297a4ed192681d9361231b53c10f
AlexisComix <Thecomixguy@hotmail.com>
2023-02-19 19:02:53 +0000

    Finish Python RLE algorithm

---
+++ alexis/code/Compression/RLE/rle.py
@@ -0,0 +1,26 @@
+from itertools import groupby
+
+def string_rle(string: str) -> list:
+    return [( character, len(list(group)) ) 
+            for character, group in groupby(string)]
+
+def fancy_string_rle(string: str) -> str:
+    out = ""
+    encoded = string_rle(string)
+    for item, count in encoded:
+        out += f"{count}{item}"
+    return out
+
+def main():
+    some_str = "11111110000000000101010000000"
+    rle = string_rle(some_str)
+    fancy = fancy_string_rle(some_str)
+    print(rle)
+    print(fancy)
+    print(compression_ratio(some_str, fancy))
+
+def compression_ratio(original: str, compressed: str) -> float:
+    return len(compressed) / len(original)
+
+if __name__ == "__main__":
+    main()
